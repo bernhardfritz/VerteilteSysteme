6e89f56abb894e68b81f7acd148265bcce260001
Pati <patrick.deutsch@outlook.com>
2016-05-10 16:12:35 +0200

    init

---
+++ Assignment8/src/chord/INode.java
@@ -0,0 +1,9 @@
+package chord;
+
+public interface INode {
+
+	public void join();
+	
+	public void sendMSG(String msg, int target);
+	
+}
--- Assignment8/src/chord/Node.java
+++ Assignment8/src/chord/Node.java
@@ -1,5 +1,58 @@
 package chord;
 
-public class Node {
+public class Node implements INode {
+
+	public static final int m = 5;
+
+	private int id;
+	private Node successor;
+	private Node predecessor;
+
+	public int getId() {
+		return id;
+	}
+
+	public Node getSuccessor() {
+		return successor;
+	}
+
+	public void setSuccessor(Node successor) {
+		this.successor = successor;
+	}
+
+	public Node getPredecessor() {
+		return predecessor;
+	}
+
+	public void setPredecessor(Node predecessor) {
+		this.predecessor = predecessor;
+	}
+
+	// create new Node and return reference
+	public static Node getNodeInstance(int id) {
+
+		//check valid id
+		if (id >= Math.pow(id, m) || id < 0) {
+			return null;
+		}
+
+		//create new node
+		return new Node(id);
+	}
+
+	// constructor
+	private Node(int id){
+		this.id = id;
+	}
+
+	@Override
+	public void join() {
+
+	}
+
+	@Override
+	public void sendMSG(String msg, int target) {
+
+	}
 
 }
